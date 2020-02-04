e9fd703ab4422771dc453dfb2ac269270f996530
aozde <a.ozder@outlook.com>
2020-02-04 16:43:07 -0600

    new message added

--- src/Udemy/AlmagulJava.java
+++ src/Udemy/AlmagulJava.java
@@ -3,6 +3,7 @@ package Udemy;
 public class AlmagulJava {
     public static void main(String[] args) {
         System.out.println ("Almagul cok iyisin");
+        System.out.println ("Hi there!");
 
     }
 }
