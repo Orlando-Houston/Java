d0bd6786402d1f6c1bcd707f088bb1ceb0505e74
aozde <a.ozder@outlook.com>
2020-02-07 14:43:02 -0600

    exam

--- src/Oracle/O79Arr.java
+++ src/Oracle/O79Arr.java
@@ -2,13 +2,15 @@ package Oracle;
 
 public class O79Arr {
     public static void main(String[] args) {
-        int [][] num={{1,3},{2,3}};
-
+        int [][] num={{1,3},
+                      {2,3}};
         for (int i=num.length-1;i>=0;i--){
-            for (int j=num.length-1;i>=0;i--)
-
-                System.out.println (num[i][j]);
+            for (int newList:num[i]){
+                System.out.println (newList);
             }
         }
+
     }
+        }
+
 
