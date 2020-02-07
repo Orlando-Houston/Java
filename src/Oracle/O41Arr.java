18840d2fec411b94eeed4c1443ef9f1cfe57d1e8
aozde <a.ozder@outlook.com>
2020-02-07 13:06:36 -0600

    exam

---
+++ src/Oracle/O41Arr.java
@@ -0,0 +1,9 @@
+package Oracle;
+
+public class O41Arr {
+    public static void main(String[] args) {
+        String[] plnt={"Mercury","Venus","Earth","Mars"};
+        System.out.println (plnt.length);
+        System.out.println (plnt[2].length ());
+    }
+}
