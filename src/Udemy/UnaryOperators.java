df2958fd14694a5d2ac78f0ec5a89a43deccad2a
aozde <a.ozder@outlook.com>
2020-02-17 20:36:28 -0600

    exam

---
+++ src/Udemy/UnaryOperators.java
@@ -0,0 +1,14 @@
+package Udemy;
+
+public class UnaryOperators {
+    public static void main(String[] args) {
+        int e=3;
+        int f=++e *5 / e-- + --e;
+        System.out.println ("e="+e+"f="+f);
+
+        //e=3
+        //4*5/2+
+
+
+    }
+}
