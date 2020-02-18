edf85aff2a5f34009c804561ab2ba259a32a7cb9
aozde <a.ozder@outlook.com>
2020-02-18 10:34:42 -0600

    exam

---
+++ src/Udemy/OrderingElements.java
@@ -0,0 +1,19 @@
+package Udemy;
+import  java.util.*;
+
+class MyOtherClass{}
+class OtherClass{}
+
+public class OrderingElements {
+    int myInt=10;
+
+
+    public static void main(String[] args) {
+        int anotherInt=20;
+
+      //  void myMethod(){}
+
+
+}}
+//PIC(package,import,class)
+
