0d656647a7955ef9640b42d65fca1838458fd3fb
aozde <a.ozder@outlook.com>
2020-02-17 20:52:10 -0600

    exam

--- src/Udemy/StaticImport.java
+++ src/Udemy/StaticImport.java
@@ -1,12 +1,14 @@
 package Udemy;
 
 import static Udemy.Config.*;
+import static java.lang.Math.PI;
 import static java.lang.Math.min;
 
 public class StaticImport {
     public static void main(String[] args) {
         int min=min(5,7);
         System.out.println ("min="+min);
+        System.out.println (PI);
 
 
         printConfig();//because  it is static
