1f79f6f4d9a7010a3694e441920b7dbf217b17bf
aozde <a.ozder@outlook.com>
2020-02-17 21:36:25 -0600

    exam

---
+++ src/Udemy/DeclaringAndInitializingVariables.java
@@ -0,0 +1,54 @@
+package Udemy;
+
+public class DeclaringAndInitializingVariables {
+    public static void main(String[] args) {
+        //declaring and initializing in to lines
+
+        int myNumber;  //declaration
+       // System.out.println (myNumber);//compile error
+        myNumber=10; //initialization
+        System.out.println (myNumber);
+
+
+        //
+        double myDouble=7.50;
+        System.out.println (myDouble);
+
+        //
+        float myFloat1,myFloat2,myFloat3;
+        float myFloat4;
+        float myFloat5;
+
+        float myFloat6=5f,myFloat7=10f,myFloat8;
+        boolean b1,b2;
+
+
+       // double d1,double d2;//dosn't compile
+
+        int i1;
+        int i2;
+
+       // int i3;i4//dosn't compile
+
+       // int int=10; not compile
+        char cHaR;
+        char Char;
+
+
+        float okFloat;
+        double $Ok2Double1;
+        double _alsoDouble;
+        float _OkButNotNice$_123;
+
+        //Illegal examples
+        //double 3point;
+        //double my@street;
+        //float *$coffee;
+        //float double;
+        //double public;
+
+
+
+
+    }
+}
