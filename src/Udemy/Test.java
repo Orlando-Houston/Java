0c9267e0d55556bc85d0f84a6fe2025e46674e4f
aozde <a.ozder@outlook.com>
2020-02-19 22:42:26 -0600

    exam

---
+++ src/Udemy/Test.java
@@ -0,0 +1,10 @@
+package Udemy;
+
+public class Test {
+    public static void main(String[] args) {
+        // IS-A
+        SuperVisor spvz=new SuperVisor ("Zeynep Ozder",9000,"IT",3);
+        spvz.showTheInformation ();
+        spvz.increase (2000);
+    }
+}
