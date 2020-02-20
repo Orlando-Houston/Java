087db33cce7dbff203bcb417a3fec76f37e86c1a
aozde <a.ozder@outlook.com>
2020-02-19 22:59:57 -0600

    exam

---
+++ src/Udemy/Animal.java
@@ -0,0 +1,28 @@
+package Udemy;
+
+public class Animal {
+    private int age;
+    private String name;
+
+    public Animal(int age){
+        System.out.println ("Animal");
+        this.setAge (age);
+
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
