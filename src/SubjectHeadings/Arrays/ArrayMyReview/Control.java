4f05ceccca7593fcce5a1627477ac3d0ca648389
aozde <a.ozder@outlook.com>
2020-06-12 18:52:06 -0400

    Repeat

---
+++ src/SubjectHeadings/Arrays/ArrayMyReview/Control.java
@@ -0,0 +1,11 @@
+package SubjectHeadings.Arrays.ArrayMyReview;
+
+public class Control {
+
+        public static void main(String[] args) {
+            if(false)
+                System.out.println("A");
+            System.out.println("B");
+
+        }
+}
